2fe23aa52bead71adfb887233c75bdb7f02c062c
rombam <romanballano@gmail.com>
2023-04-13 12:23:13 +0200

    feat: add post stuff

--- src/dustutils/post.py
+++ src/dustutils/post.py
@@ -131,8 +131,66 @@ class Hinge(PrintAnalysis):
 
 @dataclass
 class Probe(PrintAnalysis):
-    """Probe analysis."""
-    pass
+    """Probe analysis.
+
+    Attributes
+    ----------
+    name : str
+        Name of the analyis, will be appended as a suffix to the basename.
+    start_res : int
+        First result to be visualized.
+    end_res : int
+        Last result to be visualized.
+    variable : str, list
+        Variable(s) to be visualized.
+    input_type : str
+        The way probing points are specified. Can be 'point_list' for a series of points
+        specified directly in the input file or 'from_file' to load the list of points
+        from a formatted ascii file.
+    point : ProbePoint, list(ProbePoint), optional
+        Probe point(s) to be used. Required if input_type is 'point_list'.
+    file : str, Path, optional
+        Path to the file containing the list of points. Required if input_type is 'from_
+        file'.
+    step_res : int, optional
+        Step between results to be visualized. Default is 1.
+    format : str, optional
+        Format of the output file. Default is 'vtk', can be 'vtk' or 'tecplot'.
+    average : bool, optional
+        If True, will average the results. Default is False.
+    component : str, list, optional
+        Name(s) of the component(s) to be postprocessed. Default is 'all'.
+    reference_tag : str, optional
+        Tag of the reference frame to compute the loads in. Default is '0', which corres-
+        ponds to the global reference frame.
+
+    """
+    name: str
+    start_res: int
+    end_res: int
+    variable: Union[Literal['vorticity', 'vorticity_vector', 'velocity',
+                            'surface_velocity', 'pressure', 'cp', 'turbulent_viscosity',
+                            'vortex_rad'],
+                    List[str]]
+    n_xyz: Union[List[int], np.ndarray]
+    min_xyz: Union[List[Union[int, float, np.number]], np.ndarray]
+    max_xyz: Union[List[Union[int, float, np.number]], np.ndarray]
+    step_res: int = 1
+    format: Literal['vtk', 'tecplot'] = 'vtk'
+    average: bool = False
+    component: Union[str, List[str]] = 'all'
+    reference_tag: str = '0'
+
+    def __post_init__(self):
+        """Post-constructor method to assign fixed values."""
+        # Prepare the target attribute list in correct order
+        attlist = deepcopy(list(self.__dict__.keys()))
+        attlist.insert(0, 'type')
+
+        # Reconstruct and reorder attributes
+        self.type = 'flow_field'
+        self.__dict__ = {attlist[i]: self.__dict__[attlist[i]]
+                         for i in range(len(attlist))}
 
 
 @dataclass
