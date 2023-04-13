98c2ee94e76b4a37a2f82a243595c859559d4737
rombam <romanballano@gmail.com>
2023-04-13 10:49:19 +0200

    feat: add flow_field postprocessing

--- src/dustutils/post.py
+++ src/dustutils/post.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from dataclasses import dataclass
 from typing import List, Union, Literal
 from pathlib import Path
@@ -135,8 +137,64 @@ class Probe(PrintAnalysis):
 
 @dataclass
 class FlowField(PrintAnalysis):
-    """Flow analysis."""
-    pass
+    """Flowfield analysis.
+
+    Attributes
+    ----------
+    name : str
+        Name of the analyis, will be appended as a suffix to the basename.
+    start_res : int
+        First result to be visualized.
+    end_res : int
+        Last result to be visualized.
+    variable : str, list
+        Variable(s) to be visualized.
+    n_xyz : list, np.ndarray
+        Number of points in each direction. For a plane or a line, insert 1 point in the
+        applicable direction.
+    min_xyz : list, np.ndarray
+        Minimum coordinates of the flowfield.
+    max_xyz : list, np.ndarray
+        Maximum coordinates of the flowfield.
+    step_res : int, optional
+        Step between results to be visualized. Default is 1.
+    format : str, optional
+        Format of the output file. Default is 'vtk', can be 'vtk' or 'tecplot'.
+    average : bool, optional
+        If True, will average the results. Default is False.
+    component : str, list, optional
+        Name(s) of the component(s) to be postprocessed. Default is 'all'.
+    reference_tag : str, optional
+        Tag of the reference frame to compute the loads in. Default is '0', which corres-
+        ponds to the global reference frame.
+
+    """
+    name: str
+    start_res: int
+    end_res: int
+    variable: Union[Literal['vorticity', 'vorticity_vector', 'velocity',
+                            'surface_velocity', 'pressure', 'cp', 'turbulent_viscosity',
+                            'vortex_rad'],
+                    List[str]]
+    n_xyz: Union[List[int], np.ndarray]
+    min_xyz: Union[List[Union[int, float, np.number]], np.ndarray]
+    max_xyz: Union[List[Union[int, float, np.number]], np.ndarray]
+    step_res: int = 1
+    format: Literal['vtk', 'tecplot'] = 'vtk'
+    average: bool = False
+    component: Union[str, List[str]] = 'all'
+    reference_tag: str = '0'
+
+    def __post_init__(self):
+        """Post-constructor method to assign fixed values."""
+        # Prepare the target attribute list in correct order
+        attlist = deepcopy(list(self.__dict__.keys()))
+        attlist.insert(0, 'type')
+
+        # Reconstruct and reorder attributes
+        self.type = 'flow_field'
+        self.__dict__ = {attlist[i]: self.__dict__[attlist[i]]
+                         for i in range(len(attlist))}
 
 
 @dataclass
